43de76175e92b5119a56bd5a9fec2888ee9877ae
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2022-04-19 19:37:13 +0100

    add new methods

--- extractor.py
+++ extractor.py
@@ -3,29 +3,6 @@ import pandas as pd
 import os
 import classes
 import re
-# this part goes do main.py
-# Excel file location
-file_location = 'data/raw/Data_Example.xlsx'
-# create one dataframe for the Information sheet information in the excel file
-raw_information_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='Information')
-# create one dataframe for the General_Information sheet information in the excel file
-raw_general_information_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='General_Information')
-# create one dataframe for the Network_info sheet information in the excel file
-raw_network_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='Network_Info')
-# create one dataframe for the Peers_info sheet information in the excel file
-raw_peers_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='Peers_Info')
-# create one dataframe for the Load sheet information in the excel file
-raw_load_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='Load')
-# create one dataframe for the Generator sheet information in the excel file
-raw_generator_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='Generator')
-# create one dataframe for the Storage sheet information in the excel file
-raw_storage_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='Storage')
-# create one dataframe for the Vehicle sheet information in the excel file
-raw_vehicle_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='Vehicle')
-# create one dataframe for the CStation sheet information in the excel file
-raw_charging_station_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='CStation')
-# create one dataframe for the Network_Info sheet information in the excel file
-raw_network_info_sheet = pd.read_excel(os.getcwd() + '\\' + file_location, sheet_name='Network_Info')
 ##############################################################################
 #################### Extractor Functions: Peers ##############################
 ##############################################################################
